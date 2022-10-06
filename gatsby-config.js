6573d01dcd4f8f136a8920acb61e563addf080e5
david <tdsemakula@gmail.com>
2022-10-06 06:34:03 +0300

    update sitemap last modified

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         output: '/sitemap',
         serialize: ({ path, modifiedGmt }) => ({
           url: path,
-          lastmod: modifiedGmt ? modifiedGmt : '2022-09-29',
+          lastmod: modifiedGmt ? modifiedGmt : '2022-10-06',
         }),
       },
     },
