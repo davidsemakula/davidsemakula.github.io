5dba632a90d19b8444551a381831cc32184d70f5
davidsemakula <hello@davidsemakula.com>
2022-11-13 21:24:35 +0300

    manifest updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,10 @@ module.exports = {
         name: 'davidsemakula',
         short_name: 'davidsemakula',
         start_url: '/',
-        background_color: '#663399',
+        background_color: '#fafafa',
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: '#4f46e5',
         display: 'minimal-ui',
         icon: 'src/images/rocket.svg', // This path is relative to the root of the site.
       },
