29e71a268bdc2068735236596a653c00f50c37dc
davidsemakula <hello@davidsemakula.com>
2023-03-13 17:34:49 +0300

    add asset redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,9 @@ exports.createPages = async ({ actions }) => {
     fromPath: '/safe-hyperlane-router',
     toPath: 'https://safe.mukutu.tech',
   });
+  createRedirect({
+    fromPath: '/ink-analyzer/w3f/architecture-diagram',
+    toPath:
+      'https://docs.google.com/drawings/d/e/2PACX-1vRe7A3hKARyqpSJFDRgi2BqBClnRFziuTkEK9HnHqL9MZQy_uLiLycHBv8_8oYo1Mol28L-12qnFpPA/pub?w=960&h=720',
+  });
 };
